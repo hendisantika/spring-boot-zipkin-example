fb4940a8f334ad028b0f800c59d85842c947b623
Hendi Santika <hendisantika@gmail.com>
2021-07-31 11:44:59 +0700

    #4 - Initial Project for spring-boot-zipkin-example-service-2

---
+++ spring-boot-zipkin-example-service-2/src/main/java/com/example/zipkinservice2/SpringBootZipkinExampleService2Application.java
@@ -0,0 +1,13 @@
+package com.example.zipkinservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZipkinExampleService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZipkinExampleService2Application.class, args);
+    }
+
+}
