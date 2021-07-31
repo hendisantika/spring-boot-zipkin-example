21900069ca7326c37df344afd6deb952bbecee24
Hendi Santika <hendisantika@gmail.com>
2021-07-31 11:31:04 +0700

    #1 - Initial Project for spring-boot-zipkin-example-service-1

---
+++ spring-boot-zipkin-example-service-1/src/main/java/com/hendisantika/zipkinservice1/SpringBootZipkinExampleService1Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.zipkinservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZipkinExampleService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZipkinExampleService1Application.class, args);
+    }
+
+}
---
+++ spring-boot-zipkin-example-service-1/src/test/java/com/hendisantika/zipkinservice1/SpringBootZipkinExampleService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.zipkinservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZipkinExampleService1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
